4616cabaa84b9150a10511d61142a7babc9f8818
Arthur Furukawa <arthurrfurukawa@hotmail.com>
2023-11-23 13:44:25 -0300

    Atualização com Códigos do Ryuiti

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ public class Main {
         Scanner input = new Scanner(System.in);
         String command = "i";
         while(!command.equals("o")) {
-            System.out.println("digite seu comando:");
+            System.out.println("Digite seu Comando:");
             command = input.nextLine();
 
             if (command.startsWith(":e")) {
@@ -100,7 +100,82 @@ public class Main {
                     while (current != list.getHead());
                 }
             }
+
+
+            // Colocar os dois ":/" para serem uma operação só, mas diferentes ao mesmo tempo
+            if (command.startsWith(":/")){
+                String element = command.split(" ")[1];
+                if (list == null){
+                    System.out.println("Lista ainda nao foi criada utilize o comando (:e) primeiro\n");
+                }else{
+                    System.out.println("Procurando pelo elemento: " + element);
+                    Node current = list.getHead();
+                    int line = 1;
+                    do {
+                        if (current.getData().contains(element)){
+                            System.out.println(line + ". " + current.getData());
+                        }
+                        current = current.getRight();
+                        line++;
+                    } while (current != list.getHead());
+                }
+            }
+
+
+            if (command.startsWith(":/")){
+                String[] commandContent = command.split(" ");
+                String elem = commandContent[1];
+                String elemTroca = commandContent[2];
+                if(list == null){
+                    System.out.println("Lista ainda nao foi criada utilize o comando (:e) primeiro\n");
+                }else{
+                    System.out.println("Procurando pelo elemento: " + elem + " para substituir por: " + elemTroca);
+                    Node current = list.getHead();
+                    int line = 1;
+                    do {
+                        if (current.getData().contains(elem)){
+                            current.setData(current.getData().replace(elem, elemTroca));
+                            System.out.println("Elemento substituído na linha " + line + ". " + current.getData());
+                        }
+                        current = current.getRight();
+                        line++;
+                    } while (current != list.getHead());
+                }
+            }
+
+            if (command.startsWith(":a")){
+                int posLin = Integer.parseInt(command.split(" ")[1]);
+                if (list == null){
+                    System.out.println("Lista ainda nao foi criada utilize o comando (:e) primeiro\n");
+                }else{
+                    System.out.println("Digite as novas linhas. Após digitar as novas linhas, utilize ':a' em uma linha vazia.");
+                    String newLine = input.nextLine();
+                    while (!newLine.equals(":a")){
+                        list.insertAt(posLin, newLine);
+                        newLine = input.nextLine();
+                        posLin++;
+                    }
+                    System.out.println("Novas linhas adicionadas com sucesso.");
+                }
+            }
+
+            if (command.startsWith(":i")){
+                String[] commandContent = command.split(" ", 3);
+                int posLin = Integer.parseInt(commandContent[1]);
+                String newLine = commandContent[2];
+                if(list == null){
+                    System.out.println("Lista ainda nao foi criada utilize o comando (:e) primeiro\n");
+                }else{
+                    System.out.println("Inserindo a nova linha na posição: " + posLin);
+                    if(posLin > 1){
+                        posLin = posLin - 2;
+                    }
+                    list.insertAt(posLin, newLine);
+                    System.out.println("Novas linhas adicionadas com sucesso.");
+                }
+            }
         }
         System.out.println("\nObrigado por usar nosso programa :)\n");
     }
 }
+
